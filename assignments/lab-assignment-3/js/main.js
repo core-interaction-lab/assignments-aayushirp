bf04530cf95385be6c17bcd3f13f5bb7bcc26014
aayushirp <pitta179@newschool.edu>
2022-04-01 03:38:39 -0400

    initial changes

---
+++ assignments/lab-assignment-3/js/main.js
@@ -0,0 +1,66 @@
+const fetchSongs = async () => {
+    const response = await fetch('https://api.airtable.com/v0/appLXR09f8EqbBV79/Table%201?api_key=keyul7nEZff6Uw7lV').then(data => data.json());
+
+    console.log(response);
+
+    const songsContainer = document.getElementById('songs-container');
+
+    response.records.forEach(songs => {
+        console.log(songs.fields);
+        const articleEl = document.createElement('article');
+        const titleEl = document.createElement('div');
+        const releasedateEl = document.createElement('div');
+        const genreEl = document.createElement('div')
+        const spotifyUrlEl = document.createElement('a');
+        const songcoverEl = document.createElement('div');
+        const descriptionEl = document.createElement('div');
+
+        var img = document.createElement("song-cover");
+        img.src = "https://dl.airtable.com/.attachmentThumbnails/b62c025994af6faf7162abf18af6b36c/058a0ce3";
+        img.style.width = '400px';
+
+        var div = document.getElementById("songs-container");
+    
+
+        titleEl.innerHTML = songs.fields.Title;
+        titleEl.classList.add('title');
+
+        releasedateEl.innerHTML = songs.fields.Release_Date;
+        releasedateEl.classList.add('release-date');
+
+        genreEl.innerHTML = songs.fields.Genre;
+        genreEl.classList.add('genre');
+
+        descriptionEl.innerHTML = songs.fields.Description;
+        descriptionEl.classList.add('description');
+
+        songcoverEl.innerHTML = songs.fields.Song_Cover;
+        songcoverEl.classList.add('song-cover');
+
+        spotifyUrlEl.href = songs.fields.Spotify_url;
+        spotifyUrlEl.target = "_blank";
+        spotifyUrlEl.classList.add('spotify-link');
+        spotifyUrlEl.innerHTML = "Spotify Link";
+
+        articleEl.append(titleEl, genreEl, spotifyUrlEl, releasedateEl, descriptionEl, songcoverEl);
+
+        songsContainer.appendChild(articleEl);
+    });
+
+    const linkTags = document.querySelectorAll('.spotify-link');
+    console.log(linkTags);
+
+    linkTags.forEach((link, index) => {
+        const linkColor = link.style.color;
+        link.id = 'my-link-$(index + 1)';
+        link.addEventListener('mouseover', (evt) => {
+            link.style.color = "lightgreen";
+        });
+
+        link.addEventListener('mouseout', (evt) => {
+            link.style.color = "white";
+        });
+    });
+};
+
+fetchSongs();
