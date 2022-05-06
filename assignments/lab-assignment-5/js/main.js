2e685a4b28f643c9ebf090c0a219c8b6890bb342
aayushirp <pitta179@newschool.edu>
2022-05-06 10:12:15 -0400

    initial changes

---
+++ assignments/lab-assignment-5/js/main.js
@@ -0,0 +1,3 @@
+const todoTextarea = document.getElementById('todo-textarea');
+const addTodoBtn = document.getElementById('add-todo-btn');
+const todoContainer = document.getElementById('todo-container');
--- assignments/studio-assignment-4.1/js/main.js
+++ assignments/studio-assignment-4.1/js/main.js
@@ -25,18 +25,22 @@ document.addEventListener('keydown', function(e) {
   if (e.keyCode == 67) {
     document.getElementById('audio2').play();
     document.getElementById('image-2').style.opacity = 1;
+    document.getElementById('mood-2').style.opacity = 1;
   }
   else{
     document.getElementById('audio2').pause();
     document.getElementById('image-2').style.opacity = 0;
+    document.getElementById('mood-2').style.opacity = 0;
   }
 
   if (e.keyCode == 84) {
     document.getElementById('audio3').play();
     document.getElementById('image-3').style.opacity = 1;
+    document.getElementById('mood-3').style.opacity = 1;
   }
   else{
     document.getElementById('audio3').pause();
     document.getElementById('image-3').style.opacity = 0;
+    document.getElementById('mood-3').style.opacity = 0;
   }
 });
