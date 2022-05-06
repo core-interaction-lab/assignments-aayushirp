11ef0986150468d2fa3fca3594ea3fc76e34fd75
aayushirp <pitta179@newschool.edu>
2022-05-05 22:33:21 -0400

    initial changes

--- assignments/studio-assignment-4.1/js/main.js
+++ assignments/studio-assignment-4.1/js/main.js
@@ -13,22 +13,30 @@
 document.addEventListener('keydown', function(e) {
   if (e.keyCode == 87) {
     document.getElementById('audio').play();
+    document.getElementById('image-1').style.opacity = 1;
+    document.getElementById('mood-1').style.opacity = 1;
   }
   else{
     document.getElementById('audio').pause();
+    document.getElementById('image-1').style.opacity = 0;
+    document.getElementById('mood-1').style.opacity = 0;
   }
 
   if (e.keyCode == 67) {
     document.getElementById('audio2').play();
+    document.getElementById('image-2').style.opacity = 1;
   }
   else{
     document.getElementById('audio2').pause();
+    document.getElementById('image-2').style.opacity = 0;
   }
 
   if (e.keyCode == 84) {
     document.getElementById('audio3').play();
+    document.getElementById('image-3').style.opacity = 1;
   }
   else{
     document.getElementById('audio3').pause();
+    document.getElementById('image-3').style.opacity = 0;
   }
 });
